b35811efd516de5c6e42ea756a502e9a10a7233c
Matthieu <matt136@hotmail.fr>
2021-02-26 16:37:17 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=*, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="#!">Réservation</a></li>
+            <li><a href="#!">Promotion</a></li>
+            <li><a href="#!">Camping-car</a></li>
+            <li><a href="#!">Partenaires</a></li>
+            <li><a href="#!">Support client</a></li>
+            <li><a href="#!">COVID-19 mise à jour</a></li>
+            <li><a href="#!">Avis des clients</a></li>
+        </ul>
+        <ul>
+            <li><a href="#!"><i class="fas fa-user"></i> Mon compte</a></li>
+            <li><a href="#!"><i class="fas fa-car"></i> Gérer ma réservation</a></li>
+        </ul>
+    </nav>
+    <header>
+        <div class="headerleft">
+            <img src="./img/fra.jpg" alt="">
+            <a href="#!">FRANCE</a>
+        </div>
+        <div class="headerright">
+            <p>CONTACTEZ NOS SPECIALISTES</p>
+            <a href="#!">+33 974 592 518</a>
+        </div>
+    </header>
+
+    <section>
+        <div class="titre">
+            <h1>VENTE EXCLUSIVE ! <br> JUSQU'A -30%</h1>
+            <h3>Conduisez prudemment pendant la crise du COVID-19. En savoir <a href="#!">plus ici</a></h3>
+        </div>
+        <form>
+            <div class="form1">
+
+                <legend>CHERCHER & COMPARER LES PRIX DE LOCATION</legend>
+                <div class="">
+
+                    <input type="checkbox" id="check1">
+                    <label for="check1"> Lieu de restitution différent</label>
+                </div>
+            </div>
+
+            <label for="lieu">LIEU DE PRISE EN CHARGE:</label>
+            <div class="form2">
+                <input type="text" class="lieu">
+                <input type="text" class="lieu">
+                <input type="text" class="lieu"> <br> <br>
+            </div>
+
+            <div class="form3">
+
+                <input class="date" type="date" name="" id="">
+                <input class="date" type="time" name="" id="">
+                <input class="date" type="date" name="" id="">
+                <input class="date" type="time" name="" id="">
+            </div>
+
+            <div class="form4">
+                <div class="">
+                    <input type="checkbox" id="check2">
+                    <label for="check2">Age du conducteur 26-29 <a href="#!"><i class="fas fa-info-circle"></i></a></label>
+                </div>
+                <button class="btn-jaune">VOIR NOS OFFRES</button>
+            </div>
+
+
+        </form>
+    </section>
+
+    <div class="image">
+        <div class="logo">
+            <img src="./img/alamo.png" alt="logo">
+            <img src="./img/avis.png" alt="logo">
+            <img src="./img/budget.png" alt="logo">
+            <img src="./img/dollar.png" alt="logo">
+            <img src="./img/europcar.png" alt="logo">
+            <img src="./img/hertz.png" alt="logo">
+            <img src="./img/national.png" alt="logo">
+            <img src="./img/sixt.png" alt="logo">
+        </div>
+        <img class="abs" src="./img/car_hero.png" alt="">
+    </div>
+
+    <div class="avis">
+        <div class="etoiles">
+            <div class="star">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>" Longue expérience Toujours satisfait "</p>
+            <p>GERARD</p>
+        </div>
+        <div class="etoiles">
+            <div class="star">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>" RAS "</p>
+            <p>DIDIER</p>
+        </div>
+
+        <div class="res">
+            <div class="resl">
+                Pourquoi réserver avec nous
+            </div>
+            <div class="res2">
+                <p><i class="fas fa-check-circle"></i>Comparez les loueurs et trouvez le meilleur tarif</p>
+                <p><i class="fas fa-check-circle"></i>Annulation gratuite jusqu'à 48 heures avant la prise en charge</p>
+                <p><i class="fas fa-check-circle"></i>Pas de frais de carte de crédit</p>
+                <p><i class="fas fa-check-circle"></i>Joignables du lundi au samedi</p>
+                <p><i class="fas fa-check-circle"></i>24 000 agences à travers le monde</p>
+            </div>
+        </div>
+
+    </div>
+
+
+
+</body>
+
+</html>
